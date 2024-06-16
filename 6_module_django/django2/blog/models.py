5eeaaac87a14014e698491e54b5eecfc523eba36
qospanulan <kospanovulan@gmail.com>
2024-06-16 10:50:39 +0500

    custom user 1

--- 6_module_django/django2/blog/models.py
+++ 6_module_django/django2/blog/models.py
@@ -1,6 +1,13 @@
+from django.contrib.auth import models
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from django.contrib.auth import get_user_model
+
+
+class User(AbstractUser):
+    pass
+
 
 
 class Category(models.Model):
